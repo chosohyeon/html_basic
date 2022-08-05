3e2583382d187d76ccaf18b08096b8f24e0923d2
Hyeonkkyu <101560173+Hyeonkkyu@users.noreply.github.com>
2022-08-05 17:20:52 +0900

    22.08.05

--- project/src/App.js
+++ project/src/App.js
@@ -1,21 +1,23 @@
 import './css/basic.css';
 import './css/main.css';
 
-import { BrouserRouter, Routers, Router } from 'react-router-dom';
+// import { BrouserRouter, Routers, Router } from 'react-router-dom';
 
-import Nav from './doc/Nav';
+// import Nav from './doc/Nav';
 
 import Section01 from './doc/Section01';
 import Section02 from './doc/Section02';
 import Section03 from './doc/Section03';
+import Section04 from './doc/Section04';
 
 const App = () => {
   return (
     <>
-    <Nav />
+    {/* <Nav /> */}
     <Section01 />
     <Section02 />
     <Section03 />
+    <Section04 />
     </>
   )
 }
---
+++ project/src/doc/Section04.js
@@ -0,0 +1,24 @@
+const Section04 = () => {
+    return (
+        <section className="About02 basicSection">
+            <h2><span>CEO'S</span> GREETINGS</h2>
+            <p>항상 새로운 지구별 회사</p>
+            <div className="inner">
+                <div className="top">
+                    <figure>
+                        <img></img>
+                    </figure>
+                </div>
+                <div className="left">
+                    50%
+                </div>
+                <div className="right">
+                    50%
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default Section04;
