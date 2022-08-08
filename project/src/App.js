a30225f10250243ea472497daa8691f2cc993d1b
Hyeonkkyu <101560173+Hyeonkkyu@users.noreply.github.com>
2022-08-08 10:31:03 +0900

    22.08.08

--- project/src/App.js
+++ project/src/App.js
@@ -1,24 +1,28 @@
 import './css/basic.css';
 import './css/main.css';
 
-// import { BrouserRouter, Routers, Router } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// import Nav from './doc/Nav';
+import Nav from './doc/Nav'
 
 import Section01 from './doc/Section01';
 import Section02 from './doc/Section02';
 import Section03 from './doc/Section03';
 import Section04 from './doc/Section04';
 
+
+
 const App = () => {
   return (
-    <>
-    {/* <Nav /> */}
-    <Section01 />
-    <Section02 />
-    <Section03 />
-    <Section04 />
-    </>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Section04 />} />
+        <Route path='/section01' element={<Section01 />} />
+        <Route path='/section02' element={<Section02 />} />
+        <Route path='/section03' element={<Section03 />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
